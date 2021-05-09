85727b9d46b5bf3cf4f918c3b82581b3297c6cf2
Mridula bandi(mridula.bandi) <mridula.bandi@infosys.com>
2021-05-09 12:25:24 +0530

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by mysmac_admin on 09/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
